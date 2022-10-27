3c215b10af54feb0c574eb12a3396c1dfc86a48e
rezaulislamtarek <mrezaulislam.t@gmail.com>
2022-10-27 17:50:32 +0600

    unnessary class deleted

--- app/src/main/java/com/diatomicsoft/baserecyclerview/ListClass.kt
+++
@@ -1,6 +0,0 @@
-package com.diatomicsoft.baserecyclerview
-
-import com.diatomicsoft.baserecyclerview.base_recycler_view.BaseAdapter
-import com.diatomicsoft.baserecyclerview.base_recycler_view.ListItemViewModel
-
-data class ListClass(var list:List<UserModel>, var adapter: BaseAdapter<UserModel>)
